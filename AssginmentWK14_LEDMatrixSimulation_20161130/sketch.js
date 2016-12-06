78487f10b377c0884302d9e241f6bcddb580fc7e
ZhengrongHu <z.hu1@umiami.edu>
2016-12-06 00:04:58 -0500

    Add files via upload

---
+++ AssginmentWK14_LEDMatrixSimulation_20161130/sketch.js
@@ -0,0 +1,37 @@
+// -, -, -, -
+//R,G,B,A
+//method to get the index
+// (x, y) >>> x + y * width *4
+
+var video; 
+var scalar = 10;
+
+function setup(){
+  createCanvas( 600, 480); 
+  pixelDensity(1); 
+  video = createCapture(VIDEO);
+  video.size (width/scalar, height/scalar); 
+}
+
+function draw(){
+  background(51); 
+  video.loadPixels();
+  loadPixels(); 
+  for ( var y = 0; y < video.height; y ++){
+    for ( var x = 0; x < video.width; x ++){
+      var index = (x + y * video.width)*4;
+      var r = video.pixles[index]; 
+      var g = video.pixles[index+1]; 
+      var b = video.pixles[index+2];
+      
+      var bright = (r+g+b)/3;
+      fill(bright); 
+      ellipse(x, y, scalar, scalar);
+      // pixels[index] = bright; 
+      // pixels[index+1] = bright; 
+      // pixels[index+2] = bright;
+      // pixels[index+3] = 255; 
+    }
+    updatePixels();
+  }
+}
